8857004742b86d2963f97b6659b960042e5673ee
Liz Loomans <162842106+Liziloo@users.noreply.github.com>
2025-01-14 12:38:15 -0500

    Get rid of flex shrink on image containers

--- src/modules/carousel.js
+++ src/modules/carousel.js
@@ -1,26 +1,3 @@
-export { Carousel };
+export { carousel };
 
-class Carousel {
-    constructor(images) {
-        this.images = images,
-        this.currentImage = this.images[0],
-        this.window = document.querySelector('.carousel-window');
-        this.carouselImage = document.createElement('img');
-    }
-
-    initiate() {
-        this.carouselImage.src = this.currentImage;
-        this.carouselImage.classList.add('carousel-image');
-        this.window.appendChild(this.carouselImage);
-    }
-
-    next() {
-        const currentIndex = this.images.indexOf(this.currentImage);
-        if (currentIndex < this.images.length()) {
-            this.currentImage = this.images[currentIndex + 1];
-        } else {
-            this.currentImage = this.images[0];
-        }
-        this.carouselImage.src = this.currentImage;
-    }   
-}
+const carousel = () => {}
